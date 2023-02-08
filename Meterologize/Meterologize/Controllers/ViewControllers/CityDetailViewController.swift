dc2974206b39d8aa5311dfa0076f3cda5e46904e
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-08 12:49:52 -0700

    Added textfields

--- Meterologize/Meterologize/Controllers/ViewControllers/CityDetailViewController.swift
+++ Meterologize/Meterologize/Controllers/ViewControllers/CityDetailViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 class CityDetailViewController: UIViewController {
 
 	// MARK: - Outlets
-	@IBOutlet weak var cityNameLabel: UILabel!
-	@IBOutlet weak var currentStatusLabel: UILabel!
-	@IBOutlet weak var currentTempLabel: UILabel!
-	@IBOutlet weak var projectedHighLabel: UILabel!
+	
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var currentStatusLabel: UILabel!
+	
+    @IBOutlet weak var cityTempTextField: UITextField!
+    @IBOutlet weak var projectedHighLabel: UILabel!
 	@IBOutlet weak var projectedLowLabel: UILabel!
     
     // MARK: - Properties
@@ -25,15 +27,17 @@ class CityDetailViewController: UIViewController {
 
 		updateViews()
     }
-
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
 	// MARK: - Methods
 	func updateViews() {
         let city = objectToRecieveTheDataFromOurPrepareForSegue
         guard let city = objectToRecieveTheDataFromOurPrepareForSegue else {return}
 
-		cityNameLabel.text = city.name
+        cityNameTextField.text = city.name
 		currentStatusLabel.text = city.currentStatus
-		currentTempLabel.text = "\(city.currentTemp)"
+        cityTempTextField.text = "\(city.currentTemp)"
 		projectedHighLabel.text = "\(city.dailyHigh)"
 		projectedLowLabel.text = "\(city.dailyLow)"
 
